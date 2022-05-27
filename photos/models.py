cf1f7bd4cfd5333f762a387b0df666db1dadb936
IanoSang <ianosang18@gmail.com>
2022-05-28 02:08:58 +0300

    feat: models-adds category and photo class tables with relationship between them

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category,null=True, on_delete=models.SET_NULL)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
