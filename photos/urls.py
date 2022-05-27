69c74d8a7b01e6b1ab9d17473472457ef289150e
IanoSang <ianosang18@gmail.com>
2022-05-27 23:51:29 +0300

    feat: caches- clears all the cahes in git for gitignore to funtion

--- photos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.gallery, name='gallery'),
-    path('photo/<str:pk>/', views.view_photo, name='photo'),
-    path('add/', views.add_photo, name='add'),
-]
