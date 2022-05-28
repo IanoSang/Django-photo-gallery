4eaf21ae6970a151cc45137d6cf206480d93aaa2
IanoSang <ianosang18@gmail.com>
2022-05-28 11:26:51 +0300

    feat: admin- models classes for the db to be accessed by the admin

--- gallery/settings.py
+++ gallery/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gallery',
         'USER': 'moringa',
-        'PASSWORD':'Access',
+        'PASSWORD': 'Access',
     }
 }
 
@@ -115,7 +115,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIAL_URL = '/images/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
