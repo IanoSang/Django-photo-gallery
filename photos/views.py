406b668738e2b9994d0d143ad32d7f402e07d0d7
IanoSang <ianosang18@gmail.com>
2022-05-28 13:15:27 +0300

    feat: views- adds queries to obtain all data from the db to be displayed in the templates

--- photos/views.py
+++ photos/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from .models import Category, Photo
 
+
 # Create your views here.
 
 def gallery(request):
     categories = Category.objects.all()
-    context = {'categories': categories}
+    photos = Photo.objects.all()
+    context = {'categories': categories, 'photos': photos}
     return render(request, 'photos/gallery.html', context)
 
 
 def view_photo(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo':photo})
 
 
 def add_photo(request):
