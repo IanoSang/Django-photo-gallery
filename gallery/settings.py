d906bbcd744bda99850913bf1cbfbeb614bb89bb
IanoSang <ianosang18@gmail.com>
2022-05-28 12:05:04 +0300

    feat: base- adds a loop to the side bar to display db categories quirred in views

--- gallery/settings.py
+++ gallery/settings.py
@@ -123,6 +123,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
