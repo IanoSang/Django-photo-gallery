fe93ebeec0031d2bd5ca0b9cbc5e1e27dd8730f0
IanoSang <ianosang18@gmail.com>
2022-05-29 18:02:22 +0300

    feat: admin- registers the locaation model in the admin to be displayed in the admin page

--- photos/admin.py
+++ photos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Photo
+from .models import Category, Photo, Location
 
 admin.site.register(Category),
 admin.site.register(Photo),
+admin.site.register(Location),
