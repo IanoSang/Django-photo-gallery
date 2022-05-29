e87d987da730184a4f85c5794c5ba9ef0f3ead11
IanoSang <ianosang18@gmail.com>
2022-05-29 22:51:39 +0300

    feat: urls- adds a urls path to the search page

--- photos/urls.py
+++ photos/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('photo/<str:pk>/', views.view_photo, name='photo'),
     path('add/', views.add_photo, name='add'),
-    path('search/', views.search_results, name='search_results'),
+    path('search/', views.search_results, name='search'),
 ]
